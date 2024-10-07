7cdf30573236d60912567e7d5d06e4d546319039
chrisDCP <christopherdarielc@gmail.com>
2024-10-06 23:11:35 -0600

    Subiendo todo

---
+++ fireconfig.py
@@ -0,0 +1,6 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate('./firebase-credentials.json')
+firebase_admin.initialize_app(cred,{'databaseURL': 'https://harmony-space-59144-default-rtdb.firebaseio.com/'})
+
