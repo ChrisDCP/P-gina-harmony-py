3a132111a2233745b0854d669ce16d76f82d56f9
ChrisDCP <christopherdarielc@gmail.com>
2024-10-12 00:24:09 -0600

    letting functional the proyect, ready to implment the crud

--- fireconfig.py
+++ fireconfig.py
@@ -1,5 +1,5 @@
 import firebase_admin
-from firebase_admin import credentials, db
+from firebase_admin import credentials,db
 
 cred = credentials.Certificate('./firebase-credentials.json')
 firebase_admin.initialize_app(cred,{'databaseURL': 'https://harmony-space-59144-default-rtdb.firebaseio.com/'})
